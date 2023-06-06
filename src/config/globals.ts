477c50a30946c854ab98b583b0a6b5c37b3a2d4b
hugostanley <stanley.hugo@edufied.edu.vn>
2023-06-06 15:13:24 +0800

    feat: add globals file

---
+++ src/config/globals.ts
@@ -0,0 +1,11 @@
+export const globals = {
+  BACKEND_BASE_URL: "http://localhost:4000",
+  BE_ENDPOINTS: {},
+  FE_ENDPOINTS: {
+    LOGIN: ""
+  },
+  RESPONSE_MESSAGES: {
+    AUTHENTICATION: ["Failed to authenticate", "Incorrect token given", "Authorization headers not found"]
+  }
+
+}
