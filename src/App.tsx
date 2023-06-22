49b3dd9703993f94e602070c5f57a6db470ba390
hugostanley <stanley.hugo@edufied.edu.vn>
2023-06-22 21:27:43 +0800

    style: update styles for frinds

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import { Outlet, useLoaderData, useNavigate } from "react-router-dom"
+import { Outlet, useLoaderData, useLocation, useNavigate, useSearchParams } from "react-router-dom"
 import { globals } from "./config/globals"
-import { Squirrel } from "lucide-react"
+import { Squirrel, UserPlus } from "lucide-react"
 import { Users } from "lucide-react"
+import { useEffect } from "react"
+import { ArrowLeft } from "lucide-react"
 
 function App() {
   const data = useLoaderData()
@@ -19,13 +21,28 @@ function App() {
 
 function Header() {
   const navigate = useNavigate()
+  const searchParams = useLocation()
+  useEffect(() => {
+    console.log(searchParams.pathname)
+  }, [searchParams])
   return (
     <div className="flex justify-between p-4">
-      <div className="flex gap-1 cursor-pointer" onClick={() => navigate("/")}>
-        <Squirrel size={25} />
-        <p className="font-extrabold self-end">WYD</p>
-      </div>
-      <Users size={25} className="cursor-pointer" onClick={() => navigate(globals.FE_ENDPOINTS.FRIENDS)} />
+      {searchParams.pathname === globals.FE_ENDPOINTS.FRIENDS ? (
+        <>
+          <div className="flex gap-1 cursor-pointer" onClick={() => navigate("/")}>
+            <ArrowLeft size={25} />
+          </div>
+          <UserPlus size={25} className="cursor-pointer" />
+        </>
+      ) : (
+        <>
+          <div className="flex gap-1 cursor-pointer" onClick={() => navigate("/")}>
+            <Squirrel size={25} />
+            <p className="font-extrabold self-end">WYD</p>
+          </div>
+          <Users size={25} className="cursor-pointer" onClick={() => navigate(globals.FE_ENDPOINTS.FRIENDS)} />
+        </>
+      )}
     </div>
   )
 }
