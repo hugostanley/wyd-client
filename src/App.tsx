9785ad0a4818145a2452aebcd74546e672b29ece
hugostanley <stanleyhugo06@gmail.com>
2023-06-22 11:26:08 +0800

    feat add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,32 @@
-import { Link, Outlet, useLoaderData } from "react-router-dom"
+import { Outlet, useLoaderData, useNavigate } from "react-router-dom"
 import { globals } from "./config/globals"
+import { Squirrel } from "lucide-react"
+import { Users } from "lucide-react"
 
 function App() {
   const data = useLoaderData()
+
+  if (!data) {
+    return <Outlet />
+  }
   return (
     <>
-      {data && (
-        <div className="w-screen">
-          <h1 className="font-bold text-5xl">WYD</h1>
-          <Link to={globals.FE_ENDPOINTS.FRIENDS}>Friends</Link>
-          <Link to={"/"}>Feed</Link>
-        </div>
-      )}
+      <Header />
       <Outlet />
     </>
+  )
+}
+
+function Header() {
+  const navigate = useNavigate()
+  return (
+    <div className="flex justify-between p-4">
+      <div className="flex gap-1 cursor-pointer" onClick={() => navigate("/")}>
+        <Squirrel size={25} />
+        <p className="font-extrabold self-end">WYD</p>
+      </div>
+      <Users size={25} className="cursor-pointer" onClick={() => navigate(globals.FE_ENDPOINTS.FRIENDS)} />
+    </div>
   )
 }
 
