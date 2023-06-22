b49a7fec3c377eb20316403b4f296f9b6756fdc5
hugostanley <stanleyhugo06@gmail.com>
2023-06-22 21:27:43 +0800

    style: update styles for frinds

--- src/pages/Friends.tsx
+++ src/pages/Friends.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react"
 import { useFetch } from "../hooks/useFetch"
 import { globals } from "../config/globals"
+import { User } from "lucide-react"
 
 export default function Friends({ socket }: { socket: any }) {
   const user = JSON.parse(localStorage.getItem("user") || "{}")
@@ -11,6 +12,7 @@ export default function Friends({ socket }: { socket: any }) {
   const { data: allIncomingData, fetch: incomingFetch } = useFetch("get")
   const { data: allFriendRequests, fetch: friendRequestsFetch } = useFetch("get")
   const { data: acceptRequestData, fetch: acceptRequestFetch } = useFetch("post")
+  const [selectedTab, setSelectedTab] = useState<"pending" | "friends" | "requests">("friends")
   const [pendingFriends, setPendingFriends] = useState([])
 
   useEffect(() => {
@@ -60,26 +62,103 @@ export default function Friends({ socket }: { socket: any }) {
 
   return (
     <>
+      <div className="w-full flex flex-col items-center ">
+        <div className="justify-self-center font-bold text-2xl">
+          <h1>{user.username}</h1>
+        </div>
+        <div className="flex w-full justify-around">
+          <div className={`py-2 flex justify-center ${selectedTab === "friends" && "border-b-4 border-b-indigo-500"}`} onClick={() => setSelectedTab("friends")}>
+            <p>Friends</p>
+          </div>
+          <div className={`py-2 flex justify-center ${selectedTab === "pending" && "border-b-4 border-b-indigo-500"}`} onClick={() => setSelectedTab("pending")}>
+            <p>Pending</p>
+          </div>
+          <div className={`py-2 flex justify-center ${selectedTab === "requests" && "border-b-4 border-b-indigo-500"}`} onClick={() => setSelectedTab("requests")}>
+            <p>Requests</p>
+          </div>
+        </div>
+        <div className="border-t border-t-slate-300 w-full">
+          {selectedTab === "pending" && (
+            <div>
+              {pendingFriends && pendingFriends.map(item => {
+                if (item.requester._id === user._id) {
+                  return (
+                    <div className="flex p-4 gap-2">
+                      <div className="w-10 h-10 border border-slate-300 rounded-full flex justify-center items-center">
+                        <User size={30} />
+                      </div>
+                      <div className="flex flex-col justify-center">
+                        <p className="font-bold text-lg leading-[0.8]">{item.recipient.username}</p>
+                        <p className="text-sm">{item.recipient.email}</p>
+                      </div>
+                    </div>
+                  )
+                }
+              })}
+            </div>
+          )}
+
+          {selectedTab === "requests" && (
+            <div>
+              {pendingFriends && pendingFriends.map(item => {
+                if (item.recipient._id === user._id) {
+                  return (
+                    <div className="flex p-4 gap-2">
+                      <div className="w-10 h-10 border border-slate-300 rounded-full flex justify-center items-center">
+                        <User size={30} />
+                      </div>
+                      <div className="flex flex-col justify-center grow">
+                        <p className="font-bold text-lg leading-[0.8]">{item.recipient.username}</p>
+                        <p className="text-sm">{item.recipient.email}</p>
+                        <div className="mt-2 flex gap-2 grow">
+                          <button className="bg-indigo-500 grow rounded-lg px-3 py-2 text-sm text-white" onClick={() => handleAcceptRequest(item)}>Confirm</button>
+                          <button className="bg-gray-400 grow rounded-lg px-3 py-2 text-sm text-white" onClick={() => { }}>Delete</button>
+                        </div>
+                      </div>
+                    </div>
+                  )
+                }
+              })}
+            </div>
+          )}
+          {selectedTab === "friends" && (
+            <div>
+              {allFriends && allFriends.map(item => {
+                if (item.recipient.username !== user.username) {
+                  return (
+                    <div className="flex p-4 gap-2">
+                      <div className="w-10 h-10 border border-slate-300 rounded-full flex justify-center items-center">
+                        <User size={30} />
+                      </div>
+                      <div className="flex flex-col justify-center">
+                        <p className="font-bold text-lg leading-[0.8]">{item.recipient.username}</p>
+                        <p className="text-sm">{item.recipient.email}</p>
+                      </div>
+                    </div>
+                  )
+                }
+
+                if (item.requester.username !== user.username) {
+                  return (
+                    <div className="flex p-4 gap-2">
+                      <div className="w-10 h-10 border border-slate-300 rounded-full flex justify-center items-center">
+                        <User size={30} />
+                      </div>
+                      <div className="flex flex-col justify-center">
+                        <p className="font-bold text-lg leading-[0.8]">{item.requester.username}</p>
+                        <p className="text-sm">{item.requester.email}</p>
+                      </div>
+                    </div>
+                  )
+                }
+              })}
+            </div>
+          )}
+
+        </div>
+      </div>
       {user && (
         <>
-          <div className="font-bold">All Friends</div>
-          {allFriends && allFriends.map(item => {
-            if (item.recipient.username !== user.username) {
-              return (
-                <div className="flex border-[1px] border-gray-500 gap-5">
-                  <h1>{item.recipient.username}</h1>
-                </div>
-              )
-            }
-
-            if (item.requester.username !== user.username) {
-              return (
-                <div className="flex border-[1px] border-gray-500 gap-5">
-                  <h1>{item.requester.username}</h1>
-                </div>
-              )
-            }
-          })}
           <div className="font-bold mt-5">All users</div>
           {allUsers && allUsers.data.users.map(item => {
             return (
@@ -89,29 +168,6 @@ export default function Friends({ socket }: { socket: any }) {
                 <button onClick={() => handleAddClick(item)}>Add friend</button>
               </div>
             )
-          })}
-          <div className="mt-5 font-bold">incoming request</div>
-          {pendingFriends && pendingFriends.map(item => {
-            if (item.recipient._id === user._id) {
-              return (
-                <div className="flex border-[1px] border-gray-500 gap-5">
-                  <h1>{item.requester.username}</h1>
-                  <h1>{item.requester.email}</h1>
-                  <button onClick={() => handleAcceptRequest(item)}>accept</button>
-                </div>
-              )
-            }
-          })}
-          <div className="mt-5 font-bold">pending requests</div>
-          {pendingFriends && pendingFriends.map(item => {
-            if (item.requester._id === user._id) {
-              return (
-                <div className="flex border-[1px] border-gray-500 gap-5">
-                  <h1>{item.recipient.username}</h1>
-                  <h1>{item.recipient.email}</h1>
-                </div>
-              )
-            }
           })}
         </>
       )}
