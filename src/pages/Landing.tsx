e5905d48d183a9080b98154d54e049bd48e626e3
hugostanley <stanleyhugo06@gmail.com>
2023-06-22 23:42:15 +0800

    style: add gif showcase in landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,6 +1,7 @@
 import { Squirrel } from "lucide-react";
 import { Link } from "react-router-dom";
 import { globals } from "../config/globals";
+import gif from '../assets/mobile.gif'
 
 export default function Landing() {
   return (
@@ -25,7 +26,8 @@ export default function Landing() {
         <h1 className="font-bold text-5xl">Never miss a thing.</h1>
         <span className="">Do your tasks while letting your friends know.</span>
         <button className="shadow-lg text-xl font-bold bg-indigo-500 px-5 py-2 rounded-lg text-white mb-20">Get started</button>
-        <div className="w-[45rem] h-[35rem] rounded-xl shadow-2xl">
+        <div className="w-[45rem] h-[35rem] rounded-xl shadow-2xl flex justify-center items-center bg-gray-200">
+          <img src={gif} className="w-48 h-auto"/>
         </div>
       </main>
     </>
